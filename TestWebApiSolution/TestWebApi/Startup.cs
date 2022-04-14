1ce5d50fdac42ea608bae1a67cb272888799494f
ArturHorst-HahnAir <a.horst@hahnair.com>
2022-04-14 09:26:20 +0200

    deleted comment

--- TestWebApiSolution/TestWebApi/Startup.cs
+++ TestWebApiSolution/TestWebApi/Startup.cs
@@ -34,7 +34,6 @@ namespace TestWebApi
             });
         }
 
-        // Tis is a test comment
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {

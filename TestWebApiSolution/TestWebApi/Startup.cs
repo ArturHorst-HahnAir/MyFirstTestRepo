b9426525897e2f4ceaa1f83b1e6fc3e2c7013ad0
ArturHorst-HahnAir <a.horst@hahnair.com>
2022-04-14 10:09:41 +0200

    Test update Startup.cs

--- TestWebApiSolution/TestWebApi/Startup.cs
+++ TestWebApiSolution/TestWebApi/Startup.cs
@@ -34,10 +34,6 @@ namespace TestWebApi
             });
         }
 
-        
-        // Now a right comment here ... its still a test.
-
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
